99fa26ba2364c4c3e8a8144bb5ca52ff63bd8f4c
Durgesh-Selkari-1 <durgesh.selkari@iviewlabs.net>
2021-12-07 18:18:43 +0530

    feat(login page):Working on login page

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Route, Switch,BrowserRouter, Redirect} from "react-router-dom";
-
+import Landingpage from './components/Landingpage';
 import HomePage from "./containers/Home";
 
 
 export default function Routes() {
 
     return (
-        <BrowserRouter>
+       /* <BrowserRouter>
             <Switch>
                 <Redirect exact from="/" to="/home" />
                 <Route exact path="/home" component={HomePage}/>
             </Switch>
+        </BrowserRouter>*/
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Landingpage}/>
+            </Switch>
         </BrowserRouter>
         
     );
+
+
 }
---
+++ src/components/Landingpage/index.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button, FormGroup, Label, Input, Form } from 'reactstrap';
+import './index.css';
+import { GoogleLoginButton, FacebookLoginButton, LinkedInLoginButton } from 'react-social-login-buttons';
+
+export default function Landingpage() {
+
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [allEntry, setAllEntry] = useState([]);
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        const newEntry = { email: email, password: password };
+
+        setAllEntry([...allEntry, newEntry]);
+        console.log(allEntry);
+    }
+
+
+
+    return (
+        <>
+            <Form action="" onSubmit={submitForm} className="login-form" >
+                <h1>PEXit</h1>
+                <h3>Login</h3>
+
+                <FormGroup>
+
+                    <div>
+                        <Label htmlFor="email">Email</Label><br />
+                        <Input type="email" name="email" id="email" autoComplete="off" value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+
+
+
+                        />
+                    </div>
+                </FormGroup>
+
+                <FormGroup>
+                    <div>
+                        <Label htmlFor="password">Password</Label>
+                        <Input type="password" name="password" id="password" autoComplete="off" value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+
+                        />
+                    </div>
+                </FormGroup>
+                <Button type="submit">Login</Button>
+                <GoogleLoginButton />
+                <FacebookLoginButton />
+                <LinkedInLoginButton />
+
+            </Form>
+            <div>
+                {
+                    allEntry.map((curElem) => {
+
+                        return (
+                            <div className="showDataStyles">
+                                <p>{curElem.email}</p>
+                                <p>{curElem.password}</p>
+                            </div>
+                        )
+
+
+                    })
+                }
+            </div>
+
+        </>
+    )
+}
+
