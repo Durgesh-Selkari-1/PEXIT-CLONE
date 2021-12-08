f0c4a12c301903a1e19d51510ab4f2e7e473ebbe
ritesh058 <ritesh.kumar@gsolwit.com>
2021-12-08 10:11:41 +0530

    fix(folder structure): Made modification in the folder structure and fixed routing

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import { Route, Switch,BrowserRouter, Redirect} from "react-router-dom";
-import Landingpage from './components/Landingpage';
-import HomePage from "./containers/Home";
+import Landingpage from './components/LandingPage';
+import LoginPage from './components/LoginPage';
 
 
 export default function Routes() {
 
     return (
-       /* <BrowserRouter>
-            <Switch>
-                <Redirect exact from="/" to="/home" />
-                <Route exact path="/home" component={HomePage}/>
-            </Switch>
-        </BrowserRouter>*/
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Landingpage}/>
+                <Redirect exact from="/" to="/landing" />
+                <Route exact path="/landing" component={Landingpage}/>
+                <Route exact path="/login" component={LoginPage}/>
             </Switch>
-        </BrowserRouter>
-        
+        </BrowserRouter>        
     );
 
 
